3a56aed0bbdd78c317b943d07edd23a9386ed64a
Vins <v.conte01@gmail.com>
2022-04-18 00:11:11 +0200

    JUnit

---
+++ src/test/java/com/example/demo/DemoControllerTests.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DemoControllerTests {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testGet() {
+		fail("Not yet implemented");
+	}
+
+}
